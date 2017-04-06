8842c1f8150b014802171920b46ea41f6d92bbb2
alexandroidd <alexandroidcreighton@gmail.com>
2017-04-06 08:55:04 -0600

    adds incomplete homework

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,58 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-router = express.Router();
+var router = express.Router();
 
-var candies;
+var candies = [
+  {
+	id: 1,
+	name: "Chewing Gum",
+	color: "Red"
+  },
+	
+  {
+  	id: 2,
+	name: "Pez",
+	color: "Green"
+  },
+  {
+  	id: 3,
+  	name: "Marshmallow",
+  	color: "Pink"
+  },
+  {
+  	id: 4,
+  	name: "Candy Stick",
+  	color: "Blue"
+  }
+
+];
+
+// var theBestCandies = JSON.stringify(candies);
 
 //What would need to go into candies
 //in order to pass our first test?
 
-router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+router.get('/candies', function(req,res) {
+	res.json(candies);
+});
+/*
+router.get('/candies/:id', function(req,res) {
+	var candyId = req.params.id;
+
+res.send(candies[candyId-1]);
+});*/
+
+router.post('/candies', function(req,res) {
+	candies.push(req.body);
+	res.json(req.body);
 });
 
 // Fill out the rest of the routes here
 
 module.exports = router;
+
+
+
+
+
+
