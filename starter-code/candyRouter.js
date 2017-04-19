8f3b23fbdb086dd4f8a0ff298eef669c7e0ca5fe
alexandroidd <alexandroidcreighton@gmail.com>
2017-04-18 18:49:03 -0600

    adds finished hw

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -32,21 +32,43 @@ var candies = [
 //What would need to go into candies
 //in order to pass our first test?
 
+
+
+//INDEX ROUTE//
 router.get('/candies', function(req,res) {
 	res.json(candies);
 });
 
+//SHOW ROUTE//
 router.get('/candies/:id', function(req,res) {
 	var candyId = req.params.id;
 
 res.send(candies[candyId-1]);
 });
 
+//POST ROUTE//
 router.post('/candies', function(req,res) {
 	candies.push(req.body);
 	res.json(req.body);
 });
 
+//PUT ROUTE//
+router.put('/candies/:id', function(req, res) {
+  var candyId = req.params.id;
+  var candyToEdit = candies[candyId-1];
+  candyToEdit.name = req.body.name;
+  candyToEdit.color = req.body.color;
+  res.json(candyToEdit);
+
+});
+
+
+//DELETE ROUTE//
+router.delete('/candies/:id', function(req, res) {
+	var candyId = req.params.id;
+  candies.splice((candyId-1), 1);
+  res.json(candies);
+});
 // Fill out the rest of the routes here
 
 module.exports = router;
